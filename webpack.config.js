8f642815c9ff03499a063cf64dde5200615d73d8
jianqing <blessjq@gmail.com>
2017-12-25 18:48:21 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,45 @@
+const path = require('path');
 module.exports = {
   entry: {
     app: './src/main.js'
   },
   output: {
-    path: './build',
-    publicPath: '/build/',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  devServer:{
+    contentBase:'./dist',
+    compress: true,
+    port: 9091
+  },
   module: {
-    loaders: [
-      { test: /\.vue$/, loader: 'vue' },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel'},
-      { test: /\.(png|jpg)$/, loader: 'file' },
-      { test: /\.(png|jpg)$/, loader: 'url?limit=10000'},
-      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    rules:[
+      {
+        test:/\.vue$/,
+        use:[
+          'vue-loader'
+        ]
+      },
+      {
+        test: /\.css/,
+        use:[
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test:/\.(png|svg|jpg|gif)$/,
+        use:[
+          'file-loader'
+        ]
+      },
+      {
+        test:/\.js$/,
+        use:[
+          'babel-loader?cacheDirectory'
+        ]
+      }
     ]
   },
-  devtool: '#source-map'
+  devtool: 'inline-source-map'
 }
