8b56f9eed2c096364b32fcf5ec57ae307af7e2b4
jianqing <blessjq@gmail.com>
2017-12-28 14:32:17 +0800

    做一个svg动画

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <svg width="440" hight="440" viewbox="0 0 440 440">
-      <circle cx="220" cy="220" r="170" stroke-width="50" stroke="#d1d3d7" fill="none"></circle>
-      <circle cx="220" cy="220" r="170" stroke-width="50" stroke="#00a5e0" fill="none" transform="matrix(0,-1,1,0,0,440)"></circle>
+    <svg width="440" height="240" viewbox="0 0 440 440">
+      <circle cx="220" cy="220" r="170" stroke-width="50" stroke="#FF0000" fill="none"></circle>
+      <circle cx="220" cy="220" r="170" stroke-width="50" stroke="#FF7F00" fill="none" transform="matrix(0,-1,1,0,0,440)" stroke-dasharray="0 1069"></circle>
+      <!-- <circle cx="220" cy="220" r="180" stroke-width="10" stroke="#FFFF00" fill="none" transform="matrix(0,-1,1,0,0,440)" stroke-dasharray="1069 0"></circle>
+      <circle cx="220" cy="220" r="170" stroke-width="10" stroke="#00FF00" fill="none" transform="matrix(0,-1,1,0,0,440)" stroke-dasharray="1069 0"></circle>
+      <circle cx="220" cy="220" r="160" stroke-width="10" stroke="#00FFFF" fill="none" transform="matrix(0,-1,1,0,0,440)" stroke-dasharray="1069 0"></circle>
+      <circle cx="220" cy="220" r="150" stroke-width="10" stroke="#0000FF" fill="none" transform="matrix(0,-1,1,0,0,440)" stroke-dasharray="1069 0"></circle>
+      <circ -->le cx="220" cy="220" r="140" stroke-width="10" stroke="#8B00FF" fill="none" transform="matrix(0,-1,1,0,0,440)" stroke-dasharray="1069 0"></circle>
     </svg>
+    <p>拖我： <input id="range" type="range" min="0" max="100" value="0" ></p>
   </div>
 </template>
 
@@ -16,8 +22,18 @@ export default {
       msg: 'test aa'
     }
   },
-  methods: {
-    
+  mounted () {
+    if (window.addEventListener) {
+      var range = document.querySelector('#range')
+      var circle = document.querySelectorAll('circle')[1]
+      if (range && circle) {
+        range.addEventListener('change', function () {
+          var percent = this.value / 100
+          var perimeter = Math.PI * 2 * 170
+          circle.setAttribute('stroke-dasharray', perimeter * percent + ' ' + perimeter * (1 - percent))
+        })
+      }
+    }
   }
 }
 </script>
@@ -37,5 +53,12 @@ li {
 }
 a {
   color: #42b983;
+}
+input{
+  width: 300px;
+}
+circle{
+  -webkit-transition: stroke-dasharray .25s;
+  transition: stroke-dasharray .25s;
 }
 </style>
